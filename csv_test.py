5879b4a01541fe97799b1ba78a5e3fc0f9fa5ed3
Mikeias Nascimento <mikeias66@gmail.com>
2015-10-26 10:58:16 -0300

    a simple interface & the mongo alimentation

--- csv_test.py
+++ csv_test.py
@@ -1,9 +1,34 @@
-# -*- coding: utf-8 -*-
-import csv
+# encoding=utf8 
+import csv, sys, json
+from pymongo import MongoClient
+from bson import json_util
 
-csv_reader = csv.reader(open("disciplinas.csv", "r"), dialect = "excel", delimiter = ",")
+# To fix the unicode problem
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+#instance of mongoDB
+client = MongoClient()
+db = client.teste
+
+# loads a csv file
+csv_reader = csv.reader(open("disciplinas.csv", "r"), delimiter = ";")
+
+my_dict = {}
+
+# iterate in the csv reader
+for row in csv_reader:
+	my_dict[row[1]] = {}
+
+csv_reader = csv.reader(open("disciplinas.csv", "r"), delimiter = ";")
 
 for row in csv_reader:
-	for cell in row:
-		x = u"%s"%cell
-		print x
+	my_dict[row[1]].update({row[2] : row[3]})
+
+
+db.students.insert_one(my_dict)
+
+cursor = db.students.find()
+#rules
+for student in cursor:
+	print json_util.dumps(student)
